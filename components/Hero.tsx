821673d81f3cd5a678bebd3572219b9c80b2ff73
roopsagar-k <roopsagarudayar9854@gmail.com>
2024-09-12 19:55:54 +0530

    favicon update

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -20,7 +20,7 @@ export function Hero() {
     <div className="min-h-screen w-full rounded-md relative flex flex-col items-center justify-center antialiased">
       <div className="max-w-2xl mx-auto p-4">
         <motion.h1 
-          className="relative z-10 text-4xl sm:text-5xl md:text-6xl lg:text-7xl text-center font-bold mb-6 bg-clip-text text-transparent bg-gradient-to-b from-zinc-200 to-zinc-600 dark:from-zinc-200 dark:to-zinc-500"
+          className="relative z-10 text-4xl sm:text-5xl md:text-6xl lg:text-7xl text-center font-bold mb-6 bg-clip-text text-transparent bg-gradient-to-b from-zinc-700 to-zinc-500 dark:from-zinc-200 dark:to-zinc-500"
           initial={{ opacity: 0, y: -20 }}
           animate={{ opacity: 1, y: 0 }}
           transition={{ duration: 0.8 }}
