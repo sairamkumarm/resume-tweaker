504eb47c8c404caaad45c378ecde140403e17483
vanamuthuV <vanamuthuvana22@gmail.com>
2024-09-16 16:58:24 +0530

    Uncommneted Few Types in Profile/page.tsx

--- types/types.ts
+++ types/types.ts
@@ -17,57 +17,57 @@ export type BlogType = {
 //     cgpa_or_marks: string
 //   }
   
-//   export interface Project {
-//     project_name: string
-//     description: string
-//     StartDate: string
-//     EndDate: string
-//     project_url: string
-//   }
-  
-//   export interface Experience {
-//     company_name: string
-//     role: string
-//     description: string
-//     StartDate: string
-//     EndDate: string
-//   }
-  
-//   export interface SocialMediaLink {
-//     platform: string
-//     url: string
-//   }
-  
-//   export interface Reference {
-//     name: string
-//     contact: string
-//     relationship: string
-//   }
-  
-  // export interface UserData {
-  //   id: number
-  //   user_id: number
-  //   fullname: string
-  //   contact_number: string
-  //   email: string
-  //   address: string
-  //   bio: string
-  //   links: string[]
-  //   skills: string[]
-  //   role: string
-  //   experience_years: number
-  //   education_level: Education[]
-  //   certifications: string[]
-  //   languages: string[]
-  //   projects: Project[]
-  //   Experience: Experience[]
-  //   achievements: string[]
-  //   social_media_links: SocialMediaLink[]
-  //   references: Reference[]
-  //   updated_at: Date
-  //   credits: number
-  //   max_credits: number
+  // export interface Project {
+  //   project_name: string
+  //   description: string
+  //   StartDate: string
+  //   EndDate: string
+  //   project_url: string
   // }
+  
+  // export interface Experience {
+  //   company_name: string
+  //   role: string
+  //   description: string
+  //   StartDate: string
+  //   EndDate: string
+  // }
+  
+  export interface SocialMediaLink {
+    platform: string
+    url: string
+  }
+  
+  // export interface Reference {
+  //   name: string
+  //   contact: string
+  //   relationship: string
+  // }
+  
+  export interface UserData {
+    id: number
+    user_id: number
+    fullname: string
+    contact_number: string
+    email: string
+    address: string
+    bio: string
+    links: string[]
+    skills: string[]
+    role: string
+    experience_years: number
+    education_level: Education[]
+    certifications: string[]
+    languages: string[]
+    projects: Project[]
+    Experience: Experience[]
+    achievements: string[]
+    social_media_links: SocialMediaLink[]
+    references: Reference[]
+    updated_at: Date
+    credits: number
+    max_credits: number
+  }
 
   type URL = {
     href: string;
